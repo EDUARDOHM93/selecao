f3c14cf029490a97d9ca7de4dbdd0ebe1487ade1
Eduardo Martins <eduardo.hmartins93@gmail.com>
2017-04-26 11:46:04 -0300

    main.c

---
+++ teste/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main (void){
+
+	int A;
+	int B;
+	int C;
+	int D;
+
+	scanf("%d %d %d %d", &A, &B, &C, &D);
+	
+	if (B > C == D > A){
+	printf ("Valores aceitos\n"); 
+	} else {
+	printf ("Valores nao aceitos\n");
+} 
+
+
+return 0;
+
+}
